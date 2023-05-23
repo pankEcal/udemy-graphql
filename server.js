2a07c2a2b21ad211507c33bc8d9f5ab13e84ceef
likhith.st <likhith.st@enginecal.com>
2023-05-23 16:09:16 +0530

    implmement resolver

--- server.js
+++ server.js
@@ -4,10 +4,26 @@ const { graphqlHTTP } = require("express-graphql");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 
-const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const typesArray = loadFilesSync("**/*", {
+	extensions: ["graphql"],
+});
 
 const schema = makeExecutableSchema({
 	typeDefs: typesArray,
+	resolvers: {
+		Query: {
+			products: async (parent) => {
+				console.log("Getting the products...");
+				const product = await Promise.resolve(parent.products);
+				return product;
+			},
+			orders: async (parent) => {
+				console.log("Getting the orders...");
+				const orders = await Promise.resolve(parent.orders);
+				return orders;
+			},
+		},
+	},
 });
 
 const root = {
